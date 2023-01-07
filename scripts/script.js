739a7e918247193222042e729455ba486ca28bca
tomnyson <tabletkindfire@gmail.com>
2023-01-07 08:07:37 +0700

    init ema

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+import products from "../product.json" assert { type: "json" };
+
 var x = 10;
 // const y = 5;
 // y = 10;
@@ -5,7 +7,7 @@ var x = 10;
 {
   let x = 2;
   // Here x is 2
-  alert(x);
+  //   alert(x);
 }
 // Here x is 10
 // thuong
@@ -21,3 +23,90 @@ console.log(mangDuplicate);
 
 const mangB = [...mang, ...mangDuplicate];
 console.log(mangB);
+
+console.log("products", products.products);
+
+// for (const item of products.products) {
+//   if (item.price >= 50000) {
+//     console.log("item", item);
+//   }
+// }
+
+//map
+const map = new Map();
+map.set("pk001", "nguyen van a");
+map.set("pk002", "nguyen van b");
+map.set("pk003", "nguyen van c");
+console.log(map.get("pk001"));
+console.log(map.size);
+console.log(map.delete("pk001"));
+console.log(map.get("pk001"));
+console.log(map.has("pk002"));
+for (const x of map.keys()) {
+  console.log(x);
+}
+console.log(map.clear());
+
+console.log(map);
+
+// const myPromise = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     resolve("I love You !!");
+//   }, 5000);
+// });
+
+// myPromise.then(function (value) {
+//   console.log(value);
+// });
+// console.log("1+2", 1 + 2);
+const API = "http://localhost:3000";
+const callAPI = new Promise((resolve, reject) => {
+  fetch(`${API}/products`)
+    .then(function (response) {
+      resolve(response.json());
+    })
+    .catch((err) => reject("lỗi server: " + err.message));
+});
+/**
+ * lấy data của api trả về render html
+ *  document.getElementById("products").innerHTML
+ */
+callAPI.then((data) => printHtml(data)).catch((err) => console.log(err));
+
+const printHtml = (data) => {
+  const elPost = document.getElementById("items");
+  let html = "";
+  for (const item of data) {
+    html += `
+    <div id="item" class="col">
+    <div class="card shadow-sm">
+      <img src="${item.image}"/>
+        <title>${item.title}</title>
+      <div class="card-body">
+        <p class="card-text">
+          ${item.description}
+        </p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <button
+              type="button"
+              class="btn btn-sm btn-outline-secondary"
+            >
+              View
+            </button>
+            <button
+              type="button"
+              class="btn btn-sm btn-outline-secondary"
+            >
+              Edit
+            </button>
+          </div>
+          <small class="text-muted">9 mins</small>
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
+  }
+  elPost.innerHTML = html;
+};
